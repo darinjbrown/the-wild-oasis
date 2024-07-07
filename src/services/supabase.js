eaf29977493dd3a706cd9413ea036c7dfdf525cd
darinjbrown <darin.brown.cs@gmail.com>
2024-07-07 15:18:51 -0500

    finish supabase setup

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://hifliweiutjofltdpdvh.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpZmxpd2VpdXRqb2ZsdGRwZHZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAzNzg4NTQsImV4cCI6MjAzNTk1NDg1NH0.Lh25fqMS-wroQw_rcQBQj8Ya3ynhazZpURkdqIM-O8E';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
