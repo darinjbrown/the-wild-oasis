f7b0a7b0eb25831e32c9e03b54a3939e668590b5
Darin J Brown <28766590+darinjbrown@users.noreply.github.com>
2024-08-25 05:25:16 -0700

    Dashboard (#6)
    * lint issues
    
    * button updates

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable react/prop-types */
+import { useCheckout } from '../../hooks/useCheckout';
 import Button from '../../ui/Button';
 
-function CheckoutButton({ bookingId }) {
+function CheckoutButton({ id }) {
+	const { checkout, isCheckingOut } = useCheckout();
+	console.log('id', id);
 	return (
-		<Button variation='primary' size='small'>
+		<Button
+			variation='primary'
+			size='small'
+			onClick={() => checkout(id)}
+			disabled={isCheckingOut}
+		>
 			Check out
 		</Button>
 	);
